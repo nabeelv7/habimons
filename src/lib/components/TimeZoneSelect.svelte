46a01149d11fa533080625535138370e36e25e0b
nabeelv7 <nabeelsadiq.dev@gmail.com>
2025-09-13 19:17:42 +0500

    work on timezone

--- src/lib/components/TimeZoneSelect.svelte
+++ src/lib/components/TimeZoneSelect.svelte
@@ -1,21 +1,35 @@
 <script lang="ts">
 	import timezones from "$lib/timezones";
+	import * as Select from "$lib/components/ui/select/index.js";
 
 	let { user_timezone } = $props();
+	function getTimeForTimeZone(timezone: string) {
+		return new Date().toLocaleTimeString("en-US", {
+			timeZone: timezone,
+			hour: "2-digit",
+			minute: "2-digit",
+		});
+	}
 </script>
 
 <form action="?/updateTimezone" method="POST">
-	<select
-		onchange={(e) => {
-			const select = e.target as HTMLSelectElement;
-			select?.form?.requestSubmit();
-		}}
-		name="timezone"
-		id="timezone"
-		value={user_timezone}
-	>
-		{#each timezones as timezone}
-			<option value={timezone}>{timezone}</option>
-		{/each}
-	</select>
+	<Select.Root type="single">
+		<Select.Trigger
+			onchange={(e) => {
+				const select = e.target as HTMLSelectElement;
+				select?.form?.requestSubmit();
+			}}
+			name="timezone"
+			id="timezone"
+			value={user_timezone}
+		>
+			{#each timezones as timezone}
+				<Select.Item value={timezone}
+					>{timezone}, , where time is {getTimeForTimeZone(
+						timezone,
+					)}</Select.Item
+				>
+			{/each}
+		</Select.Trigger>
+	</Select.Root>
 </form>
--- src/routes/signin/timezone/+page.svelte
+++ src/routes/signin/timezone/+page.svelte
@@ -5,27 +5,9 @@
 
 	let { data }: PageProps = $props();
 	const tz = Intl.DateTimeFormat().resolvedOptions().timeZone;
-
-	function getTimeForTimeZone(timezone: string) {
-		return new Date().toLocaleTimeString("en-US", {
-			timeZone: timezone,
-			hour: "2-digit",
-			minute: "2-digit",
-		});
-	}
 </script>
 
 <div class="flex flex-col justify-center items-center min-h-screen">
 	<h1>Select your Timezone</h1>
-	<form class="w-sm" action="?/updateTimezone" method="POST">
-		<select name="timezone" id="timezone" value={tz}>
-			{#each timezones as timezone}
-				<option value={timezone}
-					>{timezone}, where time is {getTimeForTimeZone(timezone)}</option
-				>
-			{/each}
-		</select>
-
-		<button type="submit">Submit</button>
-	</form>
+	<TimeZoneSelect />
 </div>
