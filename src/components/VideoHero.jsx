554f96a25030417308296cd1d530ead65cfb4ecc
nabeelv7 <nabeelsadiq.dev@gmail.com>
2025-09-14 18:09:11 +0500

    added controls to video

--- src/components/VideoHero.jsx
+++ src/components/VideoHero.jsx
@@ -15,7 +15,7 @@ export default function VideoHero() {
       <video
         ref={videoRef}
         src="/hero-video.mp4"
-        loop
+        controls
         className="absolute top-0 left-0 w-full h-full object-cover"
       />
 
